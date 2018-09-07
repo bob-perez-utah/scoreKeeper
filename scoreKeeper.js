74d2c6cc0825cdeeb673ad3e8ee40538f5c72d81
bob-perez-utah <bperez@gw.novell.com>
2018-09-06 21:40:39 -0600

    initial commit

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -4,6 +4,7 @@ var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var resetButton = document.querySelector("#reset");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -14,6 +15,7 @@ p1Button.addEventListener("click", function() {
     if (!gameOver) {
         p1Score++;
         if (p1Score === winningScore) {
+            p1Display.classList.add("winner");
             gameOver = true;
         }
         p1Display.textContent = p1Score;
@@ -26,11 +28,23 @@ p2Button.addEventListener("click", function() {
     if (!gameOver) {
         p2Score++;
         if (p2Score === winningScore) {
+            p2Display.classList.add("winner");
             gameOver = true;
         }
         p2Display.textContent = p2Score;
     }
     
+});
+
+
+resetButton.addEventListener("click", function() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
 });
 
 
